0fae250d311ffb520ed0c05bc4798fbdbec9e7c5
Alejandro Montero <alejandro.montero@ucb.edu.bo>
2024-12-05 11:39:40 -0400

    commit tercero

--- src/sumador.test.js
+++ src/sumador.test.js
@@ -2,6 +2,6 @@ import sumar from "./sumador.js";
 
 describe("Sumar", () => {
   it("deberia sumar dos numeros", () => {
-    expect(sumar(1, 1)).toEqual(2);
+    expect(sumar(1, 112312)).toEqual(112313);
   });
 });
